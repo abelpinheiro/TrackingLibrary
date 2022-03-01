91b1377986f9378cd0351f1b7e25468d0b7c4985
Abel Figueiredo <abelpinheiro95@gmail.com>
2022-03-01 00:43:00 -0300

    Cors initial code; AppSettings class retrieving json data

--- src/services/TL.Identity.API/Controllers/AuthController.cs
+++ src/services/TL.Identity.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
